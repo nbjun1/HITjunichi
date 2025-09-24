98f2291841aabbc09670823bbdd9a135d47dd7b8
PCUser <PCUser@DESKTOP-3UQN3U9>
2025-09-24 22:36:38 +0900

    curriculum_1.20_1.22 修正1

--- Curriculum_Question_1_20_1_22/src/logic/CurrentTime.java
+++ Curriculum_Question_1_20_1_22/src/logic/CurrentTime.java
@@ -4,21 +4,22 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CurrentTime {
-	//　フィールド
-	private LocalDateTime now;
+
 	// コンストラクタ
 	public CurrentTime() {
-		this.now = LocalDateTime.now();
 	}
 	// 日時の取得とフォーマット
 	public String getTime() {
+		// 呼び出した時の現在日時を取得
+		LocalDateTime now = LocalDateTime.now();
+		
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 		
 		String date = now.format(dateFormatter);
 		String time = now.format(timeFormatter);
 		
-		return "今の現在日時は" + date + " " + time;
+		return "今の現在日時は" + date + " " + time + "です";
 	}
 
 }
